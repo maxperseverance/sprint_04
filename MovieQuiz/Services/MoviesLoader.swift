7dfa1a6046ec42b5b56653df616ac006c909ddc0
Prodigy <m.horizons@mail.ru>
2024-09-10 01:04:27 +0700

    Добавлена работа с сетью. В приложении использованы фильмы из топ-250 и самых популярных фильмов IMDb

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        let rawUrl: String = "https://tv-api.com/en/API/Top250Movies/"
+        let secretKey = "k_zcuw1ytf"
+        guard let url = URL(string: rawUrl + secretKey) else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+}
